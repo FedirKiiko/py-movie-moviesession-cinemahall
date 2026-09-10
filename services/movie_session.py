efe7f66fb4af54705246d8c64bb76078e53b5ad8
Fedir Kiiko <fedir1357@gmail.com>
2026-09-10 21:29:55 +0300

    fix

--- services/movie_session.py
+++ services/movie_session.py
@@ -32,7 +32,7 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 def update_movie_session(
         session_id: int,
-        show_time: str | None = None,
+        show_time: datetime | None = None,
         movie_id: int | None = None,
         cinema_hall_id: int | None = None,
 ) -> None:
